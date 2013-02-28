308b268c401c29357a6fecc8b2a57ece84c5c224
sbashkyrtsev <sbashkyrtsev@griddynamics.com>
2013-02-28 17:56:47 +0400

    added sample tests and failsafe

---
+++ unit-and-itests/src/main/java/poc/SomeSampleClass.java
@@ -0,0 +1,4 @@
+package poc;
+
+public class SomeSampleClass {
+}
---
+++ unit-and-itests/src/test/java/poc/UnitTest.java
@@ -0,0 +1,12 @@
+package poc;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+public class UnitTest {
+    @Test
+    public void junitTestShouldFail() throws Exception {
+        fail();
+    }
+}
