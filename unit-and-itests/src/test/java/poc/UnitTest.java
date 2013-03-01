79632f9c5668301bd9808fb7f162f3b3c9c1eed6
sbashkyrtsev <sbashkyrtsev@griddynamics.com>
2013-03-01 11:54:12 +0400

    added spring to resolve variable from command line

--- unit-and-itests/src/test/java/poc/UnitTest.java
+++ unit-and-itests/src/test/java/poc/UnitTest.java
@@ -1,12 +1,24 @@
 package poc;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/test-context.xml")
 public class UnitTest {
+
     @Test
     public void junitTestShouldFail() throws Exception {
-        fail();
+        assertEquals(
+                "If you want them to pass, then '-DunitTestResult=pass' while running unit tests",
+                "pass", unitTestResult);
     }
+
+    @Autowired
+    private String unitTestResult;
 }
