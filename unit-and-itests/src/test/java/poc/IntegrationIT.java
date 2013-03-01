083ecc43ba58d0a6f1cf69c3f7eb502e6eeb2e51
sbashkyrtsev <sbashkyrtsev@griddynamics.com>
2013-03-01 13:46:00 +0400

    added integration test

---
+++ unit-and-itests/src/test/java/poc/IntegrationIT.java
@@ -0,0 +1,22 @@
+package poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/test-context.xml")
+public class IntegrationIT {
+    @Test
+    public void integrationTest() {
+        assertEquals("If you want integration tests to pass, use '-DintegrationTestResults=pass'",
+                integrationTestResult, "pass");
+    }
+
+    @Autowired
+    private String integrationTestResult;
+}
